4b1636e161f89bb37f9b0557c16017909d437364
ryuzaki <ryuzaki>
2021-08-21 13:17:22 +0530

    timeouts

--- main.go
+++ main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"context"
 	"default/handlers"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"time"
 )
 
 func main() {
@@ -15,5 +20,38 @@ func main() {
 
 	serverMux.Handle("/", rh)
 
-	http.ListenAndServe(":5000", serverMux) // bind this port to every ip => or can explicitly say the loopback address
+	server := &http.Server{
+		Handler:      serverMux,
+		ReadTimeout:  1 * time.Second,
+		WriteTimeout: 1 * time.Second,
+		IdleTimeout:  120 * time.Second,
+		Addr:         ":5000",
+	}
+
+	go func() {
+		err := server.ListenAndServe()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	signalChannel := make(chan os.Signal)
+
+	signal.Notify(signalChannel, os.Interrupt)
+	signal.Notify(signalChannel, os.Kill)
+
+	signalChannelRes := <-signalChannel
+
+	fmt.Println("Recieved termination request, shutting down: reason =>", signalChannelRes)
+
+	timerContext, cancel := context.WithTimeout(context.Background(), 120*time.Second)
+
+	defer cancel()
+
+	err := server.Shutdown(timerContext)
+
+	if err != nil {
+		fmt.Println("Error shutting down")
+		log.Fatal(err)
+	}
 }
