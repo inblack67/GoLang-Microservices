c85842c21eb58efd8efa06cc1f8d639b8f291b59
ryuzaki <ryuzaki>
2021-08-21 12:33:34 +0530

    http server init

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
 
-func main () {
-	fmt.Println("hello worlds")
+func main() {
+	fmt.Println("Server starting...")
+
+	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
+		data, err := io.ReadAll(r.Body)
+
+		if err != nil {
+
+			http.Error(rw, "idk", http.StatusInternalServerError)
+
+			// fmt.Println("error in root path")
+			// rw.WriteHeader(http.StatusInternalServerError)
+			// return
+		}
+
+		fmt.Println("data => ", data)
+		rw.Write([]byte(data))
+	})
+
+	http.ListenAndServe(":5000", nil) // bind this port to every ip => or can explicitly say the loopback address
 }
